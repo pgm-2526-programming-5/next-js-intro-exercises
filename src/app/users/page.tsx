160d962d1c32a4454aa7ac682789dd2c5e86119c
Karel De Smet <karel.de.smet@outlook.com>
2025-09-26 21:50:15 +0200

    Oefening 2 deel 3

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,4 +1,33 @@
-export default function Users() {
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export default async function Users() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  const users: User[] = await response.json();
+
   return (
     <table className="border-spacing-x-5 border-spacing-y-2.5 border-separate text-left border-2">
       <thead>
@@ -8,46 +37,14 @@ export default function Users() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Karel De Smet</td>
-          <td>karel.desmet@arteveldehs.be</td>
-        </tr>
-        <tr>
-          <td>Barry Hawkins</td>
-          <td>barry.hawkins@arteveldehs.be</td>
-        </tr>
-        <tr>
-          <td>Jane Doe</td>
-          <td>jane.doe@arteveldehs.be</td>
-        </tr>
-        <tr>
-          <td>Mary Blige</td>
-          <td>mary.blige@arteveldehs.be</td>
-        </tr>
-        <tr>
-          <td>John Deere</td>
-          <td>john.deere@arteveldehs.be</td>
-        </tr>
-        <tr>
-          <td>Jack Black</td>
-          <td>jack.black@arteveldehs.be</td>
-        </tr>
-        <tr>
-          <td>Missy Elliott</td>
-          <td>missy.elliott@arteveldehs.be</td>
-        </tr>
-        <tr>
-          <td>Neil Robertson</td>
-          <td>neil.robertson@arteveldehs.be</td>
-        </tr>
-        <tr>
-          <td>John Higgins</td>
-          <td>john.higgins@arteveldehs.be</td>
-        </tr>
-        <tr>
-          <td>Mark Selby</td>
-          <td>mark.selby@arteveldehs.be</td>
-        </tr>
+        {users.map((user: User) => {
+          return (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
