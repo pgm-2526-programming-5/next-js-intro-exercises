7554c222ab9de844f30be816ebbe2d2af8183c3e
Karel De Smet <karel.de.smet@outlook.com>
2025-09-28 22:02:00 +0200

    Oefening deel 6

--- src/app/users/loading.tsx
+++ src/app/users/loading.tsx
@@ -1,3 +1,31 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 export default function UsersLoading() {
-  return <div>Loading ...</div>;
+  const users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return (
+    <table className="border-spacing-x-5 border-spacing-y-2.5 border-separate text-left border-2">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email address</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user: number) => {
+          return (
+            <tr key={user}>
+              <td>
+                <Skeleton />
+              </td>
+              <td>
+                <Skeleton />
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
